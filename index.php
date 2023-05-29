9cd4503ef4e27defd657f601cb92b5d4aadd5261
ihsanalidev <ihsanaliwork@gmail.com>
2023-05-29 12:37:39 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At assumenda sint, dolorem voluptatem veniam consectetur. Provident ipsa consequatur incidunt. Tempora, pariatur enim sapiente molestiae in optio impedit facilis accusantium suscipit!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
